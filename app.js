4372f82700eb761ff0180f684a06eb70c34e3441
karanbadhwar <karanbadhwar07@gmail.com>
2023-09-14 14:32:15 -0400

    Swagger Docs

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 //ConnectDB
 const connectDB = require("./db/connect");
 
@@ -36,6 +41,12 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+//Routes with Router
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authentication, jobsRouter);
 
